2013261918ef971074400d1b6134d5e74d5c604a
Joel Saavedra <joelgsaavedra@gmail.com>
2025-11-26 16:16:58 -0300

    agregar redireccion

--- parcial/settings.py
+++ parcial/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "inseguro")   #'django-insecure-1y#6n8+-*e66)m2qb0ciqbmst42ni=5knq-hy5b%d$1=bgf%5)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'  #False
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'  #False,
 
 ALLOWED_HOSTS = ["twoparcial-programacion4-joel-saavedra.onrender.com"]
 
--- parcial/urls.py
+++ parcial/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("alumnos/", include("alumnos.urls")),
     path("scraper/", include("scraper.urls")),
+    path("", lambda request: redirect("dashboard")),
 ]
